731562658ee8f4ee36ba1f9e5a6e1eb88f5d1762
lmj29 <2790697508@qq.com>
2020-03-03 21:09:29 +0800

    学生管理系统

---
+++ js/ajax.js
@@ -0,0 +1,12 @@
+let devUrl= 'https://douban-api.zce.now.sh';
+let testUrl='https://douban-api.uieee.com';
+let proUrl='https://douban-api.now.sh';
+//正在上映的电影
+let baseUrl =proUrl;
+
+//正在上映
+ let in_theaters = '${baseUrl}/v2/movie/in_theaters';
+ //即将上映
+  let coming_soon ='${baseUrl}/v2/movie/coming_soon';
+//新片榜
+  let new_movie ='${baseUrl} /v2/movie/new_movie';
